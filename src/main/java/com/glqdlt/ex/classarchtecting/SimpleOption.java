3bac38005d627aa2a41331aa5fa68235b8a691f5
jhun <jhun@nm-4ones.com>
2019-03-29 14:58:59 +0900

    init

---
+++ src/main/java/com/glqdlt/ex/classarchtecting/SimpleOption.java
@@ -0,0 +1,9 @@
+package com.glqdlt.ex.classarchtecting;
+
+/**
+ * @author Jhun
+ * 2019-03-29
+ */
+public interface SimpleOption {
+    public String getOption();
+}
---
+++ src/main/java/com/glqdlt/ex/classarchtecting/StandardSimpleOption.java
@@ -0,0 +1,11 @@
+package com.glqdlt.ex.classarchtecting;
+
+/**
+ * @author Jhun
+ * 2019-03-29
+ */
+public class StandardSimpleOption implements SimpleOption {
+    public String getOption() {
+        return "options";
+    }
+}
